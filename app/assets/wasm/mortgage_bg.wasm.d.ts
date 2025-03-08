c624ff6ece17506f13fc2060a850a9c038639ccb
Mittaltiger Enterprises <mittaltigerenterprises@gmail.com>
2025-03-08 18:26:29 +0530

    added wasm build and its gluecode

---
+++ app/assets/wasm/mortgage_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const calculate_mortgage: (a: number, b: number, c: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_start: () => void;
